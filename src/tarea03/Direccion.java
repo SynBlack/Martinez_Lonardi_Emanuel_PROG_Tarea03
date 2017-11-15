b12dfd56d86983713c951ed11cf8d6ff1fd31337
Darkcrow <Darkcrow@192.168.1.102>
2017-11-15 18:22:13 +0100

    Creación enumerado Direccion

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,9 @@
+package tarea03;
+
+/**
+ *
+ * @author Emanuel Martínez Lonardi
+ * @ version 1.0
+ * @ Se crea el enumerado Direccion
+ */
+public enum Direccion {NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE};
