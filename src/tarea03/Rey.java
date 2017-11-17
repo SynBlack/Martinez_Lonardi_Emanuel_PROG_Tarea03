6f7b3743ced315e90a0658f84cb6cb038941d078
Darkcrow <Darkcrow@192.168.1.102>
2017-11-17 12:48:56 +0100

    Creación constructor clase Rey.

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -2,10 +2,28 @@ package tarea03;
 
 /**
  *
- * @author Emanuel Martínez Lonardi
- * Creamos la clase Rey con los atributos Color y posicion
+ * @author Emanuel Martínez Lonardi Creamos la clase Rey con los atributos Color
+ * y posicion
  */
 public class Rey {
+
     private Color color;
     private Posicion posicion = new Posicion();
+
+    /**
+     * Constructor que acepte como parámetros el color.
+     * @param color es un enumerado.
+     */
+    
+    public Rey(Color color) {
+        this.color = color;
+        switch (color) {
+            case BLANCO:
+                posicion = new Posicion(1, 'e');
+                break;
+            case NEGRO:
+                posicion = new Posicion(8, 'e');
+                break;
+        }
+    }
 }
