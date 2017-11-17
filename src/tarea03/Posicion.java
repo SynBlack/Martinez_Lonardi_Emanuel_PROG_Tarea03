2a48c623ed03c60032aa269ea13d512d5b887a0e
Darkcrow <Darkcrow@192.168.1.102>
2017-11-17 11:51:56 +0100

    Creación del constructor de la clase Posicion

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -2,13 +2,29 @@ package tarea03;
 
 /**
  * @author Emanuel Martínez Lonardi
- * @version 1.0
- * Se crea la clase posicion
+ * @version 1.0 Se crea la clase posicion
  */
-
 public class Posicion {
+
     // Creamos los atributos fila y columna.
     private int fila;
     private char columna;
+
+    /**
+     * Creamos el constructor de la clase Posicion a la que pasaremos los parámetros fila y columna.
+     * 
+     * @param fila recibirá el constructor un valor entero de la fila por parámetro.
+     * @param columna recibirá el constructor un valor carácter de la columna por parámetro.
+     */
     
+    public Posicion(int fila, char columna) {
+        if ((fila >= 1 && fila <= 8) && (columna >= 97 && columna <= 104)) {
+            this.fila = fila;
+            this.columna = columna;
+        } else {
+            this.fila = 1;
+            this.columna = 'a';
+            System.out.printf("Error. No se ha definido una posición válida.\n");
+        }
+    }
 }
